16492d8c34d55b50645ddda8c51b091e413af9e9
Trevor Adcock <trevor.adcock@mac.com>
2019-01-25 16:47:47 -0700

    Adds BatMobileClass

---
+++ deleteThisLater/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  deleteThisLater
+//
+//  Created by DevMountain on 1/25/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import Foundation
+
+class BatMobile{
+  
+  var wheels: Int
+  
+  func imNotWearingHockeyPads(){
+    print("I'm not wearing hockey pads")
+  }
+}
